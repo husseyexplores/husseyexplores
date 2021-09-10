bf5c3597616af3f4c0b323d2cd7c5ed4cd35961b
hsy <hsy@pop-os.localdomain>
2021-09-11 00:17:53 +0500

    Testing with content

---
+++ src/lib/components/Page.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { description as _desc, title as _title } from '$lib/meta-site-config'
+  import { fly } from 'svelte/transition'
+
+  let styles = ''
+  export { styles as class }
+
+  export let title = _title
+  export let description = _desc
+</script>
+
+<svelte:head>
+  <title>{title} • HusseyExplores</title>
+  <meta name="twitter:title" content={title} />
+  <meta property="og:title" content={title} />
+
+  <meta name="description" content={description} />
+  <meta name="twitter:description" content={description} />
+  <meta property="og:description" content={description} />
+</svelte:head>
+
+<div class="flex-grow text-0 relative">
+  <header class="sticky post-0 center center-w-3 center-g-000 | mt-2 mb-0">
+    <div class="flex-row items-center py-000 px-1">
+      <div>Maybe logo?</div>
+    </div>
+  </header>
+
+  <main
+    class={`center center-w-3 center-g-1 ${styles}`}
+    in:fly={{ y: 50, duration: 250 }}
+  >
+    <slot />
+  </main>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,24 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'
+import staticAdapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    adapter: staticAdapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+    }),
+    trailingSlash: 'always',
+    prerender: {
+      crawl: false,
+      enabled: true,
+      entries: ['*'],
+      onError: 'fail',
+    },
   },
 
   preprocess: [
@@ -15,6 +29,6 @@ const config = {
       postcss: true,
     }),
   ],
-};
+}
 
-export default config;
+export default config
