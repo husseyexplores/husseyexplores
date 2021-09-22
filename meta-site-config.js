8c0def60ebe426a8efb28f686604edcb6aa78846
hsy <hsy@pop-os.localdomain>
2021-09-22 21:36:41 +0500

    x

---
+++ meta-site-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `Husseyexplores`,
+    author: {
+      name: `Hassan`,
+      summary: `Self-taught freelance developer.`,
+    },
+    description: `Not all those who wander are lost`,
+    siteUrl: `https://husseyexplores.com/`,
+    social: {
+      twitter: `husseyexplores`,
+    },
+  },
+  meta: {
+    name: `Husseyexplores`,
+    short_name: `Husseyexplores`,
+    start_url: `/`,
+    background_color: `#ffffff`,
+    theme_color: `#fff7fa`,
+    display: `minimal-ui`,
+    icon: `content/assets/icon.png`,
+  },
+};
--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,10 @@
-import preprocess from 'svelte-preprocess'
-import staticAdapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: staticAdapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-    }),
-    trailingSlash: 'always',
-    prerender: {
-      crawl: false,
-      enabled: true,
-      entries: ['*'],
-      onError: 'fail',
-    },
   },
 
   preprocess: [
@@ -29,6 +15,6 @@ const config = {
       postcss: true,
     }),
   ],
-}
+};
 
-export default config
+export default config;
